fa065e05fbfed13a00f0781124218782173140c1
HadiAnik <hadiuzzamananik@gmail.com>
2023-04-09 19:40:19 +0600

    first commit

---
+++ api/server.js
@@ -0,0 +1,25 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
+// Bind the router db to the app
+app.db = router.db;
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(port, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
